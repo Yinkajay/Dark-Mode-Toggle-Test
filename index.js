b59e81c47a46d81974a8f8c054c6fa46d3ebdcfd
Adeyinka Junaid <yinkajunn@gmail.com>
2022-09-06 00:34:39 +0100

    Added toggle functionality

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 let ourContainer = document.getElementsByClassName("toggle-container")[0];
 let ourButton = document.getElementsByClassName("toggle-button")[0];
+const pageBody = document.getElementsByTagName('body')[0]
 
-ourContainer.addEventListener("click", function(){
+var darkMode = ourButton.classList.contains('active')
+
+ourContainer.addEventListener("click", () => {
   ourButton.classList.toggle('active')
-  console.log(ourButton.classList.contains('active'))
+  if(!darkMode){
+    darkMode = true
+  } else{
+    darkMode = false
+  }
+  console.log(darkMode)
 })
 
 
+
+
+function changeTheme() {
+  if (darkMode){
+    pageBody.classList.add('dark-bg')
+  }else{
+    pageBody.classList.remove('dark-bg')
+  }
+}
+
+ourContainer.addEventListener("click", changeTheme
+)
